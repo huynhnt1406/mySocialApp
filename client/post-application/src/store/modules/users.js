2f8da10df2e58ee7baeb2b3b7e810ea4e29d985d
Nguyen Thanh Huynh CMC-GLOBAL DU11 <nthuynh@cmc.com.vn>
2021-05-26 17:23:25 +0700

    add user-management

---
+++ client/post-application/src/store/modules/users.js
@@ -0,0 +1,21 @@
+
+
+const state = {
+    user : []
+}
+
+const getters = {
+    allUsers : state => state.users
+}
+
+const actions = {
+    
+}
+
+const mutations = {
+
+}
+
+export default {
+    state, getters,actions, mutations
+}
